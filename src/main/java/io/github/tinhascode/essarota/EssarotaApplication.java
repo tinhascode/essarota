b0c78c1ef75fe6e9f63d6625a6a3bd7d940ce661
tinhascode <victormartinssantos.work@gmail.com>
2026-09-13 20:53:16 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/io/github/tinhascode/essarota/EssarotaApplication.java
@@ -0,0 +1,13 @@
+package io.github.tinhascode.essarota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssarotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssarotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/tinhascode/essarota/EssarotaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tinhascode.essarota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssarotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
